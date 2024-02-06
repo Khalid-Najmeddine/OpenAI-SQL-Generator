d853683e971a95d30e398b9e10b107184699b3d4
Khalid Najmeddine <knajm574@gmail.com>
2024-02-05 18:03:46 -0700

    feat: display search query messages as a scrollable list

--- src/components/OneMessageDisplay.tsx
+++ src/components/OneMessageDisplay.tsx
@@ -1,11 +1,19 @@
+import { FcApproval } from "react-icons/fc";
 
+interface UserMessage {
+  role: string,
+  content: string
+}
 
-const OneMessageDisplay = () => {
+interface MessageDisplayProps {
+  message: UserMessage
+}
+
+const OneMessageDisplay = ({message}:MessageDisplayProps) => {
   return (
     <div className="one-Message-Display">
-      <p id="icon">X</p>
-      <p>User</p>
-      <p>Message</p>
+      <p id="icon"><FcApproval size={25}/></p>
+      <p>{message.content}</p>
       
     </div>
   )
